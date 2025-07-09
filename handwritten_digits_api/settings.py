2a8161304a89dd7e8a9c34cd2fee69f7b1d30a36
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-07-09 08:36:26 +0530

    Update settings.py

--- handwritten_digits_api/settings.py
+++ handwritten_digits_api/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'handwritten_digits_api.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
+        os.environ.get("DATABASE_URL") + "?sslmode=require", 
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True
